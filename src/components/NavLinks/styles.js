999ec644d5b678322ff7370ab6110c10d96daf99
LukasLimalkl <lukinhaslima063@gmail.com>
2023-05-18 17:58:30 -0300

    mobile menu builded

--- src/components/NavLinks/styles.js
+++ src/components/NavLinks/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.a`
     display: flex;
     justify-content: flex-end;
+
     a {
         text-decoration: none;
         color: #2b62f8;
